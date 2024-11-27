8830f5c32ede91aff23a5ed5a0aaf6286a95ff95
= <168827531+ZNemuZ@users.noreply.github.com>
2024-11-28 07:04:09 +0900

    環境変数とModel

---
+++ model/user.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	UserName  string    `json:"username"`
+	Email     string    `json:"email" gorm:"unique;not null"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoCreateTime"`
+	Status    uint      `json:"status"`
+}
+
+//ユーザーが作成されたときに返すレスポンスのデータの型
+type UserResponce struct {
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	UserName string `json:"username"`
+	Email    string `json:"email" gorm:"unique;not null"`
+}
