d6959509d51e229694d821bae2d9366b7cc72bea
= <168827531+ZNemuZ@users.noreply.github.com>
2024-12-08 22:21:59 +0900

    corsとcsrfの処理追加

--- router/router.go
+++ router/router.go
@@ -1,18 +1,36 @@
 package router
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/ZNemuZ/outly-back/controller"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, pc controller.IPostController) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")}, //許可するオリジンの設定
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken}, //許可するヘッダー
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		AllowCredentials: true, //認証情報を含めるリクエストを許可する
+	}))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"), //cookieの送信される範囲を指定
+		CookieHTTPOnly: true,
+		// CookieSameSite: http.SameSiteNoneMode,
+		CookieSameSite: http.SameSiteDefaultMode,
+		// CookieMaxAge: 60,
+	}))
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	e.POST("/csrf", uc.CsrfToken)
 	p := e.Group("/posts")
 	//postsに関連するエンドポイントに向かう際に必ず実行される
 	p.Use(echojwt.WithConfig(echojwt.Config{ //認証ミドルウェアに設定を渡す
